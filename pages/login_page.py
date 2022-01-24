b0e304f4cdf77ad8434f88555abccb33530398c8
myghtyDuck <vsevolodseleznev@gamil.com>
2022-01-24 15:02:47 +0300

    login page checks are added

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,13 +8,12 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.browser.current_url.find("login") != -1, "URL is not contain Login"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not displayed"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not displayed"
+
+
--- pages/test.py
+++ pages/test.py
@@ -5,4 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 browser = webdriver.Chrome()
 browser.get(link)
-browser.find_elements(By.ID, "register_form")
+#browser.find_elements(By.ID, "register_form")
+#url = browser.current_url.find("loginxui")
+assert browser.current_url.find("login") != -1, "URL is not contain Login"
